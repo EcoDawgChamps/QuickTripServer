231cc58d99252cc82e7361e7d5c17fbf6eeac60e
Ryan Lee <ryanlee@Ryans-MacBook-Pro.local>
2022-05-19 00:33:03 -0700

    find by id now works

--- routes/controllers/cars.js
+++ routes/controllers/cars.js
@@ -2,7 +2,12 @@ import express from 'express';
 var router = express.Router();
 
 router.get('/', async (req, res) => {
-    let allCars = await req.models.Car.find();
+    let allCars = [];
+    if (req.query.userID) {
+        allCars = await req.models.Car.find({owner: req.query.userID});
+    } else {
+        allCars = await req.models.Car.find();
+    }
     if (req.query.numCars) {
         allCars = allCars.slice(0, req.query.numCars);
     }
