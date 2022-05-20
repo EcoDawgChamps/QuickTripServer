15b55ba3590ff9e7833494cdc6783926bb9be8de
lavishal <ajt33@uw.edu>
2022-05-19 17:43:33 -0700

    added login/signup api

--- models.js
+++ models.js
@@ -2,68 +2,72 @@ import mongoose from "mongoose";
 
 let models = {};
 
-const main = () => {
+const main = async () => {
     console.log("connecting to MongoDB...");
-    mongoose.connect("mongodb+srv://CocoaCommander:8LkbXJp7TlMQFGXp@cluster0.2xpgo.mongodb.net/capstone?retryWrites=true&w=majority")
-        .then(console.log("connection successful!"))
-        .catch(err => console.log(err));
-    const userSchema = new mongoose.Schema({
-        firstName: String,
-        lastName: String,
-        driversLicense: String,
-        email: String,
-        password: String
-    });
+    try{
+      const result = await mongoose.connect("mongodb+srv://CocoaCommander:8LkbXJp7TlMQFGXp@cluster0.2xpgo.mongodb.net/capstone?retryWrites=true&w=majority")
+      if(result) console.log("connection successful!");
+      const userSchema = new mongoose.Schema({
+          firstName: String,
+          lastName: String,
+          driversLicense: String,
+          email: String,
+          password: String
+      });
 
-    const carSchema = new mongoose.Schema({
-        owner: mongoose.Types.ObjectId,
-        year: Number,
-        color: String,
-        VIN: String,
-        capacity: Number,
-        location: String,
-        features: [String],
-        make: String,
-        model: String
-    });
+      const carSchema = new mongoose.Schema({
+          owner: mongoose.Types.ObjectId,
+          year: Number,
+          color: String,
+          VIN: String,
+          capacity: Number,
+          location: String,
+          features: [String],
+          make: String,
+          model: String
+      });
 
-    const imageSchema = new mongoose.Schema({
-        car: mongoose.Types.ObjectId,
-        img: {
-            data: Buffer,
-            contentType: String
-        }
-    });
+      const imageSchema = new mongoose.Schema({
+          car: mongoose.Types.ObjectId,
+          img: {
+              data: Buffer,
+              contentType: String
+          }
+      });
 
-    const rentalSchema = new mongoose.Schema({
-        renter: mongoose.Types.ObjectId,
-        startDate: Date,
-        endDate: Date
-    })
+      const rentalSchema = new mongoose.Schema({
+          renter: mongoose.Types.ObjectId,
+          startDate: Date,
+          endDate: Date
+      })
 
-    const reviewSchema = new mongoose.Schema({
-        rental: mongoose.Types.ObjectId,
-        rating: Number,
-        reviewBody: String
-    })
+      const reviewSchema = new mongoose.Schema({
+          rental: mongoose.Types.ObjectId,
+          rating: Number,
+          reviewBody: String
+      })
 
-    models.User = mongoose.model('User', userSchema);
-    console.log("User Schema Created");
-    models.Car = mongoose.model('Car', carSchema);
-    console.log("Car Schema Created");
-    models.Image = mongoose.model('Image', imageSchema);
-    console.log("Image Schema Created");
-    models.Rental = mongoose.model('Rental', rentalSchema);
-    console.log("Rental Schema Created");
-    models.Review = mongoose.model('Review', reviewSchema);
-    console.log("Review Schema Created");
+      models.User = mongoose.model('User', userSchema);
+      console.log("User Schema Created");
+      models.Car = mongoose.model('Car', carSchema);
+      console.log("Car Schema Created");
+      models.Image = mongoose.model('Image', imageSchema);
+      console.log("Image Schema Created");
+      models.Rental = mongoose.model('Rental', rentalSchema);
+      console.log("Rental Schema Created");
+      models.Review = mongoose.model('Review', reviewSchema);
+      console.log("Review Schema Created");
 
-    // models.Post = mongoose.model('Post', postSchema);
-    // console.log("Post schema created.");
-    // models.Comment = mongoose.model('Comment', commentSchema);
-    // console.log("Comment schema created.");
-    // models.Info = mongoose.model('Info', infoSchema);
-    // console.log("Info schema created.");
+      // models.Post = mongoose.model('Post', postSchema);
+      // console.log("Post schema created.");
+      // models.Comment = mongoose.model('Comment', commentSchema);
+      // console.log("Comment schema created.");
+      // models.Info = mongoose.model('Info', infoSchema);
+      // console.log("Info schema created.");
+    }
+    catch(e){
+      console.log(e.message);
+    }
 }
 
 main();
--- routes/controllers/users.js
+++ routes/controllers/users.js
@@ -1,5 +1,43 @@
 import express from 'express';
 var router = express.Router();
+import bcrypt from 'bcryptjs';
+import models from '../../models.js'
 
+// // Sign up
+router.post('/signup', async (req, res) => {
+  try {
+    let user = await models.User.findOne({email: req.body.email});
+    if (user) return res.status(200).send({ error: 'Email already in use!' });
+    req.body.password = await bcrypt.hash(req.body.password, 5);
+
+    const userNew = await models.User.create(req.body);
+    if (!userNew) {
+      return res.status(200).send({ error: 'Unable to save user' });
+    }
+    return res.status(200).send(userNew);
+  } catch (e) {
+    return res.status(500).send({
+      'error': e.message
+    });
+  }
+})
+
+// Login
+router.post('/login', async(req, res) => {
+  try {
+    const user = await models.User.findOne({email: req.body.email});
+    if (!user) return res.status(200).send({ error: 'User does not exist' });
+    const pass = await bcrypt.compare(req.body.password, user.password);
+    if (!pass) return res.status(200).send({ error: 'Invalid Username or Password' });
+    const resPayload = {
+      success: true,
+    }
+    return res.status(200).send(resPayload);
+  } catch (e) {
+    return res.status(500).send({
+      'error': e.message
+    });
+  }
+})
 
 export default router;
